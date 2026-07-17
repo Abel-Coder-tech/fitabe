08ef440fc4911a7ef43ca9e5e8b99c3ed02ccb3d
Abel <osseabel@gmail.com>
2026-07-17 17:29:49 +0100

    feat(sitemap): add sitemap:generate command with spatie/laravel-sitemap

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
+
+class GenerateSitemap extends Command
+{
+    protected $signature = 'sitemap:generate';
+    protected $description = 'Generate the sitemap.xml file';
+
+    public function handle()
+    {
+        $base = config('app.url');
+
+        $pages = [
+            '/' => ['daily', '1.0'],
+            '/vote' => ['daily', '0.9'],
+            '/medias' => ['weekly', '0.8'],
+            '/contact' => ['weekly', '0.7'],
+            '/mentions-legales' => ['monthly', '0.3'],
+            '/confidentialite' => ['monthly', '0.3'],
+            '/cgu' => ['monthly', '0.3'],
+            '/reglement' => ['monthly', '0.3'],
+        ];
+
+        $sitemap = Sitemap::create();
+
+        foreach ($pages as $path => [$freq, $priority]) {
+            $sitemap->add(
+                Url::create($base . $path)
+                    ->setChangeFrequency($freq)
+                    ->setPriority($priority)
+            );
+        }
+
+        $sitemap->writeToFile(public_path('sitemap.xml'));
+
+        $this->info('Sitemap generated successfully at ' . public_path('sitemap.xml'));
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,12 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use App\Console\Commands\GenerateSitemap;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('sitemap:generate', function () {
+    $this->call(GenerateSitemap::class);
+})->purpose('Generate sitemap.xml');
