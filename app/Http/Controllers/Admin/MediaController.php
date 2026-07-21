e1236ce9acd51f26a67c8a5b9e8307b1dcb2d6ab
Abel <osseabel@gmail.com>
2026-07-21 09:28:42 +0100

    Trier l'id

--- app/Http/Controllers/Admin/MediaController.php
+++ app/Http/Controllers/Admin/MediaController.php
@@ -11,7 +11,7 @@ class MediaController extends Controller
     // Affiche la liste paginée des médias
     public function index()
     {
-        $medias = Medias::orderBy('id')->paginate(20);
+        $medias = Medias::orderBy('id', 'asc')->paginate(20);
         return view('admin.media.index', compact('medias'));
     }
 
