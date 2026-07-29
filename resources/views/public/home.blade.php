e0c91eb0c6c2461c93c7fb5d49f7cd9505b78696
Abel <osseabel@gmail.com>
2026-07-29 09:16:54 +0100

    fix(promoteurs): supprime le gap entre image et texte avec g-0

--- resources/views/public/home.blade.php
+++ resources/views/public/home.blade.php
@@ -593,7 +593,7 @@ margin-right: 0;
         </div>
 
         {{-- Promoteur 1 : image gauche, texte droite --}}
-        <div class="row align-items-center mb-5">
+        <div class="row align-items-center g-0 mb-5">
             <div class="col-lg-6 text-center text-lg-start">
                 <div class="position-relative d-inline-block" style="max-width: 500px;">
                     <img src="{{ asset('images/promoteurs/promoteur1.png') }}" alt="MISTER OKEKE" loading="lazy" width="500" height="500"
@@ -625,7 +625,7 @@ margin-right: 0;
         </div>
 
         {{-- Promoteur 2 : inversé (image droite, texte gauche) --}}
-        <div class="row align-items-center gy-4 flex-row-reverse">
+        <div class="row align-items-center g-0 flex-row-reverse">
             <div class="col-lg-6 text-center text-lg-start">
                 <div class="position-relative d-inline-block" style="max-width: 500px;">
                     <img src="{{ asset('images/promoteurs/promoteur2.png') }}" alt="EYISSE SOBUR BABATUNDE" loading="lazy" width="500" height="500"
